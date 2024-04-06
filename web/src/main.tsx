f68c235b9eab12626238fd1a241fd4a219e32d95
aryankarma <aryankarma29@gmail.com>
2024-04-06 00:27:26 -0700

    added profile

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Profile from './profile.tsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={App} />
+        <Route path="/Profile" Component={Profile} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
